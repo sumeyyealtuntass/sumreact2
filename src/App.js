dbbb514927e9a730615dfba18dfca6611cd486b6
Sümeyye Altuntaş <ssumeyye.altuntas@gmail.com>
2023-06-23 11:55:59 +0300

    kodları githuba yükledik

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+     test
+     <img src='/logo192.png'></img>
+     <img src={logo}>a</img>
+    </div>
+  );
+}
+
+export default App;
